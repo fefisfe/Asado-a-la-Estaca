17d0f76e3005ae5ae8b2bd50aed761530170748d
Estefanía Fernanda Charles Mengeón <100200049+fefisfe@users.noreply.github.com>
2022-11-09 19:57:13 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+function cambiarfondo1() { document.body.style.backgroundColor = "black";}
+
+function cambiarfondo2() {document.body.style.backgroundColor = "#C70039";}
+
+function cartel() {alert("Estefanía Charles Mengeón- Rosario, Santa Fe")}
